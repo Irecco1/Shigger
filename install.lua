8b03d36dd1688f5b96253f2525eb8ce2a6fd8c94
Irecco <154021738+Irecco1@users.noreply.github.com>
2026-02-18 12:21:44 +0100

    Replace wget commands with shell.run for downloads

--- install.lua
+++ install.lua
@@ -1,12 +1,12 @@
 print("Downloading files...")
-wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/main.lua main.lua
-wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/movement.lua movement.lua
-wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/inventory.lua inventory.lua
-wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/planner.lua planner.lua
-wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/scanner.lua scanner.lua
-wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/fuel.lua fuel.lua
-wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/state.lua state.lua
-wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/config.lua config.lua
-wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/digger.lua digger.lua
+shell.run("wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/main.lua main.lua")
+shell.run("wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/movement.lua movement.lua")
+shell.run("wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/inventory.lua inventory.lua")
+shell.run("wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/planner.lua planner.lua")
+shell.run("wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/scanner.lua scanner.lua")
+shell.run("wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/fuel.lua fuel.lua")
+shell.run("wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/state.lua state.lua")
+shell.run("wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/config.lua config.lua")
+shell.run("wget https://raw.githubusercontent.com/Irecco1/Shigger/refs/heads/main/Shigger/digger.lua digger.lua")
 print("Download complete! Now run the main file to start.")
 
