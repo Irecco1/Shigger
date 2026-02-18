11733491ae60cbeaa05968512812054bb8ed9c50
Irecco1 <154021738+Irecco1@users.noreply.github.com>
2026-02-18 11:06:27 +0100

    Add files via upload

---
+++ Shigger/main.lua
@@ -0,0 +1,67 @@
+-- main.lua
+
+local config = require("config")
+local digger = require("digger")
+local fuel = require("fuel")
+local inventory = require("inventory")
+local movement = require("movement")
+local planner = require("planner")
+local scanner = require("scanner")
+local state = require("state")
+
+
+local function emergencyReturn(err)
+    print("ERROR:", err)
+    movement.goTo({x=state.getPosition().x, y=-5, z=state.getPosition().z})
+    inventory.emptyInventory()
+end
+
+fuel.setMovementGoTo(movement.goTo)
+
+-- =====================
+-- MAIN LOGIC
+-- =====================
+
+local function main()
+    --[[
+    1. first start, it should be placed facing north, with the chest behind the robot on the same level.
+    2. next, preferably the coal should be put in the slot 1 of the turtle, for it to refuel itself
+    3. after that, the robot can start the work:
+    3.1 scan the surrounding area and look for whitelist targets
+    3.2 plan the target order using the scanner list
+    3.3 dig each target
+    3.3.1 if inventory gets full after any dig action, use inventory.lua to either throw thrash away or go back to chest and then continue work
+    3.4 go back to x=0, z=0, y=-8 (using the scan point as the reference)
+    3.5 dig 9 blocks down
+    3.6 go back to 3.1 until bedrock detected
+    3.7 if bedrock detected, save the list of the scanned targets and just leave. If allthemodium is detected, dont mine it (because it cant), just save it location and continue
+    4. after finishing the mining, go back to surface, empty everything, give the log (preferably create a txt file) of found and not mined targets and finish the program.
+    
+    If at any point, the robot encounters an error, it should immidietely go back to surface
+    --]]
+
+    fuel.manualRefuel()
+    local depth = 0
+    while true do
+        local scan_list = scanner.scan()
+        local targets = planner.makePlan(scan_list)
+        for _, target in ipairs(targets) do
+            digger.dig(target)
+            inventory.checkInventory()
+        end
+        depth = depth -9
+        movement.goTo({x=0, y=depth, z=0})
+
+        if scanner.isBedrockFound() then
+            break
+        end
+    end
+    inventory.emptyInventory()
+end
+
+
+
+local ok, err = pcall(main)
+if not ok then
+    emergencyReturn(err)
+end
---
+++ Shigger/state.lua
@@ -0,0 +1,87 @@
+-- state.lua
+
+local state = {}
+
+-- local variables
+local data = {
+    position = {x=0,y=0,z=0},
+    direction = 0,  -- 0=N, 1=E, 2=S, 3=W
+}
+
+local DIR = {
+    [0] = {x=0,z=-1},
+    [1] = {x=1,z=0},
+    [2] = {x=0,z=1},
+    [3] = {x=-1,z=0},
+}
+
+
+-- =====================
+-- PRIVATE
+-- =====================
+
+
+
+
+-- =====================
+-- PUBLIC API
+-- =====================
+
+function state.updatePosition(direction)
+    --accepts 'forward', 'back', 'up', 'down'
+    if direction == "forward" then
+        data.position.x = data.position.x + DIR[data.direction].x
+        data.position.z = data.position.z + DIR[data.direction].z
+        return
+    end
+    if direction == "back" then
+        data.position.x = data.position.x - DIR[data.direction].x
+        data.position.z = data.position.z - DIR[data.direction].z
+        return
+    end
+    if direction == "up" then
+         data.position.y = data.position.y + 1
+         return
+    end
+    if direction == "down" then
+         data.position.y = data.position.y - 1
+         return
+    end
+
+    error("Invalid direction")
+    
+ 
+end
+
+function state.updateDirection(direction)
+    -- <0 left or >0 right direction. Accepts multiple rotations at once
+    if direction < 0 then
+        for i=1, -direction do
+            data.direction = data.direction - 1
+            if data.direction < 0 then
+                data.direction = 3
+            end
+        end
+    elseif direction > 0 then
+        for i=1, direction do
+            data.direction = data.direction + 1
+            if data.direction > 3 then
+                data.direction = 0
+            end
+        end
+    end
+end
+
+function state.getPosition()
+    return {
+        x = data.position.x,
+        y = data.position.y,
+        z = data.position.z,
+    }
+end
+
+function state.getDirection()
+    return data.direction
+end
+
+return state
