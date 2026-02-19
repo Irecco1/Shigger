3281725853074b574944d2441c4a40797f7928fb
Irecco <154021738+Irecco1@users.noreply.github.com>
2026-02-19 19:40:18 +0100

    Update movement.lua

--- Shigger/movement.lua
+++ Shigger/movement.lua
@@ -25,8 +25,8 @@ local found_bedrock = false
 -- =====================
 
 function movement.goForward()
-    turtle.dig()
     inventory.throwAwayThrash()
+    turtle.dig()
     for i=1,retry_amount do
         local _, block_in_front = turtle.inspect()
         if block_in_front.name == "minecraft:bedrock" then
@@ -88,8 +88,8 @@ function movement.goBack()
 end
 
 function movement.goUp()
-    turtle.digUp()
     inventory.throwAwayThrash()
+    turtle.digUp()
     for i=1,retry_amount do
         if turtle.up() then
             state.updatePosition("up")
@@ -105,8 +105,8 @@ function movement.goUp()
 end
 
 function movement.goDown()
-    turtle.digDown()
     inventory.throwAwayThrash()
+    turtle.digDown()
     for i=1,retry_amount do
         if turtle.down() then
             state.updatePosition("down")
